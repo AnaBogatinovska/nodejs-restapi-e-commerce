88604dca6fd549942bc33ce171f80a7a18785498
Ana Bogatinovska <bogatinovska.ana@gmail.com>
2021-02-09 16:00:54 +0100

    Update Auth.js

--- src/api/routes/auth/Auth.js
+++ src/api/routes/auth/Auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require('../../controllers/auth')
+const authController = require('../../controllers/Auth')
 
 router.get('/users', authController.get_users)
 
